eb25f7dead3e7efc850357305ccb40188aa15504
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-11-24 19:03:00 -0500

    react-hooks-practice-animal-shelter/
    react-hooks-practice-animal-shelter/

--- src/__tests__/PetBrowser.test.js
+++ src/__tests__/PetBrowser.test.js
@@ -1,52 +1,24 @@
 import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import PetBrowser from "../components/PetBrowser";
+import Pet from "../components/Pet";
 
-const testPets = [
-  {
-    id: "5c142d9e-ea45-4231-8146-cccf71c704c0",
-    type: "dog",
-    gender: "male",
-    age: 4,
-    weight: 1,
-    name: "Trident",
-    isAdopted: false,
-  },
-  {
-    id: "2c902312-dfa3-446f-8b4b-5e115170d807",
-    type: "cat",
-    gender: "male",
-    age: 3,
-    weight: 1,
-    name: "Teddy",
-    isAdopted: false,
-  },
-  {
-    id: "6057de4f-6725-4b9f-a0b1-1f3bd3ad04a6",
-    type: "cat",
-    gender: "male",
-    age: 2,
-    weight: 5,
-    name: "Hemingway",
-    isAdopted: false,
-  },
-];
+test("should set a pet's adopted status to true", () => {
+  const pet = {
+    id: 1,
+    name: "Fido",
+    species: "dog",
+    adopted: false,
+  };
 
-test("renders Pet components based on its props", () => {
-  render(<PetBrowser pets={testPets} />);
-
-  for (const pet of testPets) {
-    expect(screen.queryByText(pet.name, { exact: false })).toBeInTheDocument();
-  }
-});
-
-test("passes an `onAdoptPet` callback prop to its children Pet components", () => {
   const onAdoptPet = jest.fn();
-  render(<PetBrowser pets={testPets} onAdoptPet={onAdoptPet} />);
 
-  const button = screen.queryAllByText(/Adopt pet/)[0];
-  fireEvent.click(button);
+  render(<Pet pet={pet} onAdoptPet={onAdoptPet} />);
 
-  expect(onAdoptPet).toHaveBeenCalled();
+  const adoptButton = screen.getByRole("button", { name: /adopt/i });
+
+  fireEvent.click(adoptButton);
+
+  expect(onAdoptPet).toHaveBeenCalledWith(pet.id);
+  expect(pet.adopted).toBe(true);
 });
